bad945603351fb3edc570b8b153de82e8f5ec403
Oliver Holmes <oliver.albert.holmes@gmail.com>
2022-03-07 21:51:46 +1030

    Remove refs

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -17,11 +17,9 @@ test('add nav items', async ({ page }) => {
   await page.locator('button:has-text("Edit nav items")').click();
   await page.locator('text=Add More Items').click();
   await page.locator('[aria-label="Available\\ Items"] >> text=All').click({delay: 1000});
-  await page.locator('text=Sessions CalendarSessions Calendar >> span').nth(2).click();
-  await page.locator('text=SessionsSessions >> span').nth(2).click();
-  await page.locator('text=AuthorizationsAuthorizations >> span').nth(2).click();
-  await page.locator('text=Authorization ItemsAuthorization Items >> span').nth(2).click();
-  await page.locator('button:has-text("Add 4 Nav Items")').click();
+  await page.locator('text=Test1Test1 >> span').nth(2).click();
+  await page.locator('text=Test2Test2 >> span').nth(2).click();
+  await page.locator('button:has-text("Add 2 Nav Items")').click();
   await page.locator('button:has-text("Save")').click();
 
 });
